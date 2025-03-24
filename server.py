56e54d4955411940bf6304e398b0ec52f3cbff9a
shubhamsinghrv <shubhamsinghvr@gmail.com>
2025-03-24 17:53:50 +0530

    final

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask
 from flask_cors import CORS
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, request
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
